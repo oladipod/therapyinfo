baa199b1b70531fae5fed7ac38ded1bebe168f2b
your name <34028888+oladipod@users.noreply.github.com>
2020-05-31 14:20:57 +0100

    git push origin master

--- monkey.py
+++ monkey.py
@@ -39,11 +39,11 @@ action = ActionChains(driver)
 #driver.get('https://www.psychologytoday.com/us/therapists/alabama/444145?sid=5ec54f52dfbcd&ref=1&tr=ResultsName')
 #driver.get('https://www.psychologytoday.com/es/psicologos-psicoterapeutas/comunidad-valenciana/726849?sid=5ecfe3fe4e2f5&ref=7&tr=ResultsName')
 #driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Dr.%20Dr.%20Terra%20Griffin,%20Ed.%20D.,%20LPC-S,%20NCC,%20Counselor,%20Hoover,%20AL,%2035226%20_%20Psychology%20Today.html')
-# driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Empower%20Counseling%20&%20Coaching,%20Counselor,%20Birmingham,%20AL,%2035223%20_%20Psychology%20Today.html')
-driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Kobus%20Fourie,%20Counsellor,%20Faerie%20Glen,%200081%20_%20Psychology%20Today.html')
-# 'file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Lena%20Santhirasegaram,%20Registered%20Psychotherapist,%20Toronto,%20ON,%20M4N%20_%20Psychology%20Today.html'
+#driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Empower%20Counseling%20&%20Coaching,%20Counselor,%20Birmingham,%20AL,%2035223%20_%20Psychology%20Today.html')
+#driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Kobus%20Fourie,%20Counsellor,%20Faerie%20Glen,%200081%20_%20Psychology%20Today.html')
+#driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Lena%20Santhirasegaram,%20Registered%20Psychotherapist,%20Toronto,%20ON,%20M4N%20_%20Psychology%20Today.html')
 #driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Online%20services%20during%20coronavirus,%20Juneau,%20AK,%2099801%20_%20Psychology%20Today.html')
-#driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Shenetra%20Alexander,%20Licensed%20Professional%20Counselor,%20Birmingham,%20AL,%2035218%20_%20Psychology%20Today.html')
+driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Shenetra%20Alexander,%20Licensed%20Professional%20Counselor,%20Birmingham,%20AL,%2035218%20_%20Psychology%20Today.html')
 #driver.get('file:///C:/Users/Samuel%20Ladipo/Documents/GitHub/therapyinfo/Shenetra%20Alexander,%20Licensed%20Professional%20Counselor,%20Birmingham,%20AL,%2035218%20_%20Psychology%20Today.html')
 #     phone = driver.find_element_by_xpath('//*[@id="profile-content"]/div/div[2]/div[1]/div/div/div[1]').get_attribute('addressLocality').text()
 # except Exception as error:
@@ -62,7 +62,24 @@ time.sleep(4)
 #     qual_practice = 'null'
 
 
-#def qualification_test()
+
+#PLEASE HELP MEEE
+
+
+# #qualification_test
+
+# qual_practice = 'Null'
+# qual_school = 'Null'
+# qual_gradyear = 'Null'
+# qual_licencestate = 'Null'
+# qual_licenceprovince = 'Null'
+# qual_supervisor = 'Null'
+# qual_supervisorlicence = 'Null'
+# qual_membership = 'Null'
+# qual_training = 'Null'
+# qual_degdiploma = 'Null'
+
+
 try:
     qual_element = driver.find_element_by_class_name('profile-qualifications').find_element_by_tag_name('ul').find_elements_by_tag_name('li')
     
@@ -71,60 +88,88 @@ try:
         try:
             if 'Years in Practice:' in qual_text:
                 qual_practice = qual_text[18:]
-                
-            
-            elif 'Year Graduated:' in qual_text:
-                qual_gradyear = qual_text[15:]
-
-            elif 'School:' in qual_text:
-                qual_school = qual_text[7:]   
-
-            elif 'License and State:' in qual_text:
-                qual_licencestate = qual_text[21:]
-                
-            
-            elif 'Licence and Province:' in qual_text:
-                qual_licenceprovince = qual_text[21:]
-                
-            
-            elif 'Supervisor:' in qual_text:
-                qual_supervisor = qual_text[11:]
-                
-            
-            elif 'Supervisor License:' in qual_text:
-                qual_supervisorlicence = qual_text[19:]
-                
-
-            elif 'Membership:' in qual_text:
-                qual_membership = qual_text[11:]
-                
-            
-            elif 'Training:' in qual_text:
-                qual_training = qual_text[8:]
-                
-
-            elif 'Degree / Diploma:' in qual_text:
-                qual_degdiploma = qual_text[8:]
+                print(qual_practice)
 
         except Exception as error:
-            print ('QUALIFICATION NOT FOUND')
+            try:
+                if 'Year Graduated:' in qual_text:
+                    qual_gradyear = qual_text[15:]
+                    
+            except Exception as error:   
+                try:
+                    if 'School:' in qual_text:
+                        qual_school = qual_text[7:] 
+                        print(qual_school) 
+
+                except Exception as error:   
+#                     try:
+#                         if 'License and State:' in qual_text:
+#                             qual_licencestate = qual_text[21:]
+#                             print(qual_licencestate)
+
+#                     except Exception as error:   
+#                         try:
+#                             if 'Licence and Province:' in qual_text:
+#                                 qual_licenceprovince = qual_text[21:]
+#                         except Exception as error:
+#                             try:
+#                                 if 'Supervisor:' in qual_text:
+#                                     qual_supervisor = qual_text[11:]
+#                             except Exception as error:
+#                                 try:
+#                                     if 'Supervisor License:' in qual_text:
+#                                         qual_supervisorlicence = qual_text[19:]
+#                                 except Exception as error:
+#                                     try:
+#                                         if 'Membership:' in qual_text:
+#                                             qual_membership = qual_text[11:]
+#                                             print(qual_membership)
+
+#                                     except Exception as error:
+#                                         try:
+#                                             if 'Training:' in qual_text:
+#                                                 qual_training = qual_text[8:]
+#                                                 print(qual_training)
+#                                         except Exception as error:
+#                                             try:
+#                                                 if 'Degree / Diploma:' in qual_text:
+#                                                     qual_degdiploma = qual_text[8:]
+#                                             except Exception as error:
+#                                                 try:
+#                                                     if 'Degree / Diploma:' in qual_text:
+#                                                         qual_degdiploma = qual_text[8:]
+#                                                 except Exception as error:
+#                                                     print('UNKNOWN FIELD')
+                                        
+
            
 
-except Exception as error:
-    qual_practice = 'Null'
-    qual_school = 'Null'
-    qual_gradyear = 'Null'
-    qual_licencestate = 'Null'
-    qual_licenceprovince = 'Null'
-    qual_supervisor = 'Null'
-    qual_supervisorlicence = 'Null'
-    qual_membership = 'Null'
-    qual_training = 'Null'
-    qual_degdiploma = 'Null'
+# except Exception as error:
+#     qual_practice = 'Null'
+#     qual_school = 'Null'
+#     qual_gradyear = 'Null'
+#     qual_licencestate = 'Null'
+#     qual_licenceprovince = 'Null'
+#     qual_supervisor = 'Null'
+#     qual_supervisorlicence = 'Null'
+#     qual_membership = 'Null'
+#     qual_training = 'Null'
+#     qual_degdiploma = 'Null'
 
-print(qual_practice)
-print(qual_school)
-print(qual_membership) 
-print(qual_licencestate)
+
+
+
+# #ADDITIONAL CREDENTIALS
+# addcred = driver.find_element_by_class_name('profile-additional-credentials').find_elements_by_tag_name('li')
+# for i in addcred:
+#     print(i.text.split('Certificate:'))
    
+# print (len(addcred))
 
+
+#QUALIFICATIONS, LOOKING FOR  10 FIELDS UNDER IT
+
+qual_element = driver.find_element_by_class_name('profile-qualifications').find_element_by_tag_name('ul').find_elements_by_tag_name('li')
+
+for i in qual_element:
+    qual_text = i.text
